3679d565537f0dd927a5cbbe8c2a529c71dc7de5
Fitsum Alemu <fitsumcs@gmail.com>
2026-03-09 12:41:24 +0300

    Node Js training code base

---
+++ Module1/app.js
@@ -0,0 +1,11 @@
+const mathLib = require('./mathfunc');
+ 
+// console.log("This is Node Js Training..");
+// console.log("This Is module 1 ");
+
+const addtion = mathLib.sum(12,12);
+const subt = mathLib.sub(12,6);
+ 
+console.log("The Sum is : " , addtion);
+console.log("The Sub is : ", subt );
+
---
+++ Module1/mathfunc.js
@@ -0,0 +1,16 @@
+// Adding two numbers 
+function sum(a,b)
+{
+    return a + b;
+}
+//sub
+function sub(a,b)
+{
+    return a - b;
+}
+//div.  - > 0
+//mul. -> 2 
+//reminder 2  - > 
+//even odd ->  user reminder func 
+
+module.exports = { sum, sub };
