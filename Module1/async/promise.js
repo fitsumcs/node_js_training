304015b1b32f7d8978df0052c029bfc2de212185
Fitsum Alemu <fitsumcs@gmail.com>
2026-03-10 11:00:45 +0300

    feat: add promise and fetchData function with error handling and async demonstration

---
+++ Module1/async/promise.js
@@ -0,0 +1,41 @@
+// How u create a promise 
+// const PR = new Promise((resolve, reject) => 
+// {
+//   //Logic 
+//   if(1>2) resolve({})
+//   else reject(new Error("This is error "))
+
+// })
+
+const fetchData = () => 
+{
+return new Promise((resolve, reject) =>
+{
+setTimeout(() => 
+{ 
+
+ const success = false;
+ if (success) { resolve({ id: 1, name: 'Alice' }); } 
+ else { reject(new Error('Failed to fetch')); } 
+
+}, 1000);
+
+ }); 
+};
+
+// This is not sync function 
+// U use then / async await
+fetchData()
+.then(data => 
+    {
+    console.log("The Data ");
+    console.log(data);
+    }
+)
+.catch(err => console.log(err.message));
+
+
+
+
+
+
